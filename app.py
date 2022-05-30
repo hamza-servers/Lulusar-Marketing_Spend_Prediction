2205f5e6d807414aa85a725f5b98e4f35cd93e8b
hamza-servers <106442344+hamza-servers@users.noreply.github.com>
2022-05-30 11:28:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ with st.container():
     st.write("This is inside the container")
 
     # You can call any Streamlit command, including custom components:
-    st.bar_chart(np.random.randn(50, 3))
+#     st.bar_chart(np.random.randn(50, 3))
 
 st.write("This is outside the container")
 
